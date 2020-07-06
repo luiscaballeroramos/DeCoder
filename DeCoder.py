782c8e2974918aeed172c24dba492e5e1d74168a
luiscaballeroramos <luiscaballeroramos@gmail.com>
2020-07-06 17:07:07 +0200

    Copy 2020_07_06

--- DeCoder.py
+++ DeCoder.py
@@ -1,46 +1,95 @@
+import numpy as np
 import PyPDF2
 import regex as re
 
-from DeCoder_Auxiliary import *
-
+from DeCoder_Auxiliary import (IdSystem,NameSystem,LocationSystem,SeparatorSystem,
+                               ReferenceSystem,
+                               ReferenceList)
 # Open PDF File
-pdfFile=open('../Codes&Standards/EC 1993.1.8.2005-1.pdf','rb')
+pdfFile=open('../Codes&Standards/EC 1993.1.8.2005-1.pdf','rb')#EC 1993.1.8.2005-1.pdf #AWS .d1.1.2000.pdf
 pdfReader=PyPDF2.PdfFileReader(pdfFile)
 numPages=pdfReader.numPages
 print('number of pages in the PDF document: {0}'.format(numPages))
 
+
+# ----------------------------------------------------------------------------
+# SETTING
+# ----------------------------------------------------------------------------
+# TOC settings
+tocIdentifiers=['contents','Contents','CONTENTS'
+                 'table of contents','Table of contents','Table Of Contents','TABLE OF CONTENTS']
+# Id settings
+idIndicatorRegex='\d'
+idSeparatorRegex='\.'
+idSeparator='.'
+# Name settings
+nameRegex='[\s\w]*'
+# Location settings
+locationRegex='\d+'
+gapTocReal=1
+# Separator settings
+separatorRegex='\.{2,}'
+sep='.'
+# No Matter settings
+nomatterRegex='[\s]*'
+
+numberDotRef=IdSystem(idIndicatorRegex=idIndicatorRegex,
+                      idSeparatorRegex=idSeparatorRegex,
+                      idSeparator=idSeparator)
+simpleNam=NameSystem(nameRegex)
+pageStringLoc=LocationSystem(locationRegex,gapTocReal=gapTocReal)
+dotSep=SeparatorSystem(separatorRegex)
+ECTocReferenceSystem=ReferenceSystem(idSys=numberDotRef,
+                         namSys=simpleNam,
+                         locSys=pageStringLoc,
+                         sepSys=dotSep,
+                         nomatterRegex=nomatterRegex)
+ECTocReferenceSystem.print()
+# ----------------------------------------------------------------------------
+# CONTENT & CORRECTION
+# ----------------------------------------------------------------------------
 # Pages content
 pages={}
 for pageNum in range(0,numPages):
   pdfPage=pdfReader.getPage(pageNum)
-  pages[pageNum]=pdfPage.extractText()
+  page=pdfPage.extractText()
+  # delete \n and \t
+  page=re.sub('\n',' ',page)
+  page=re.sub('\t',' ',page)
+  pages[pageNum]=page
   pass
 
-# Toc settings
-tocIdentifiers=['contents','Contents',
-                 'table of contents','Table of contents','Table Of Contents']
-noMatterRegex='[\s]*'
-noMatter=' '
-# Reference settings
-refidRegex='\d'
-refsepRegex='\.'
-refsep='.'
-# Name settings
-namRegex='[\s\w]*'
-# Location settings
-locRegex='\d+'
-# Separator settings
-sepRegex='\.{2,}'
-sep='.'
-
-numberDotRef=ReferenceSystem(refidRegex, refsepRegex, refsep)
-simpleNam=NameSystem(namRegex)
-pageStringLoc=LocationSystem(locRegex)
-dotSep=SeparatorSystem(sepRegex)
-ECTocSys=TocSystem(numberDotRef,simpleNam,pageStringLoc,dotSep,noMatterRegex,noMatter)
-# ECToc=Toc(numberDotRef,simpleNam,pageStringLoc,dotSep)
-
-# Searching TOC by Identifiers
+# Pages references correction
+pattern=re.compile(ECTocReferenceSystem.idRegex)
+for pageNum in range(0,numPages):
+  # look for reference, position
+  pagesReferences=[]
+  page=pages[pageNum]
+  while re.search(pattern,page) is not None:
+    reference=re.search(pattern,page).group()
+    begin=re.search(pattern,page).start()
+    pagesReferences.append([reference,
+                            begin,
+                            ECTocReferenceSystem.getId(reference)])
+    page=re.sub(reference,'',page,count=1)
+    pass
+  # delete references
+  pages[pageNum]=page
+  # introduce real reference in position
+  numReferences=len(pagesReferences)
+  for refNum in range(0,numReferences):
+    reference=pagesReferences[numReferences-refNum-1]
+    page=(page[:reference[1]]+
+          ECTocReferenceSystem.getId(reference[0])+
+          page[reference[1]:])
+    pass
+  # update pages with real references
+  pages[pageNum]=page
+  pass
+# ----------------------------------------------------------------------------
+# LOCATION
+# ----------------------------------------------------------------------------
+# Searching TOC's beginning by identifiers
 tocCoincidences={}
 tocTotalCoincidences={}
 for pageNum, pageContent in pages.items():
@@ -52,48 +101,36 @@ for pageNum, pageContent in pages.items():
   tocTotalCoincidences[pageNum]=sum(auxList)
   pass
 print('Number of TOC indentifiers in PDF file: {0}'.format(sum(tocTotalCoincidences.values())))
-maxTocTotalCoincidences = max(tocTotalCoincidences.values())
-print('Number of Max TOC indentifiers in One Page: {0}'.format(maxTocTotalCoincidences))
-maxTocTotalCoincidencesPages = [key  for (key, value) in tocTotalCoincidences.items() if value == maxTocTotalCoincidences]
-print('Pages with Max TOC indentifiers in One Page: {0}'.format(maxTocTotalCoincidencesPages))
+print('Number of Max TOC indentifiers in One Page: {0}'.format(max(tocTotalCoincidences.values())))
+print('Pages with Max TOC indentifiers in One Page: {0}'.format(
+  [key  for (key, value) in tocTotalCoincidences.items() if value == max(tocTotalCoincidences.values())]))
 
-# Searching TOC by tocRegex
+# Searching TOC's pages by reference system
 tocCoincidences={}
 for pageNum, pageContent in pages.items():
-  tocCoincidences[pageNum]=len(re.findall(ECTocSys.tocRegex,pages[pageNum]))
+  tocCoincidences[pageNum]=len(re.findall(ECTocReferenceSystem.referenceRegex,pageContent))
   pass
-noNullTocCoincidencesPages = [key  for (key, value) in tocCoincidences.items() if value != 0]
-print('Pages with toc Regex: {0}'.format(noNullTocCoincidencesPages))
+tocPages = [key  for (key, value) in tocCoincidences.items() if value != 0]
+print('Pages with toc Regex: {0}'.format(tocPages))
 
-# TOC pages
-tocPages=noNullTocCoincidencesPages
-
-# toc's in TOC
-pattern = re.compile(ECTocSys.tocRegex)
+# left tocs in TOC
+ECTocList=ReferenceList(ECTocReferenceSystem)
+pattern = re.compile(ECTocReferenceSystem.referenceRegex)
 for page in tocPages:
   for match in pattern.finditer(pages[page]):
-    [split1,split2]=re.split(ECTocSys.sepSys.sepRegex,match.group())
-    ref=Reference(numberDotRef,re.search(ECTocSys.refSys.refRegex,split1).group())
-    split1=re.sub(ECTocSys.refSys.refRegex,'',split1)
-    nam=Name(simpleNam,re.search(ECTocSys.namSys.namRegex,split1).group())
-    split1=re.sub(ECTocSys.namSys.namRegex,'',split1)
-    locPage=re.search(ECTocSys.locSys.locRegex,split2).group()
-    beginLocation=Location(pageStringLoc,locPage,begin=None,end=None)
-    separator=Separator(dotSep, sep)
-    ECTocUnit=TocUnit(ref,nam,beginLocation,separator)
-    TOC_EC3.addToc(ECTocUnit)
+    ECTocList.addReference(match.group(),gapActivation=True)
     pass
   pass
-TOC_EC3.tocList.sort(key=lambda x: x.ref, reverse=False)
-TOC_EC3.printTOC()
+ECTocList.sort(by='id')
 
 # Search Missing toc's in TOC
 refMissing=[]
-for toc in TOC_EC3.tocList:
-  levels=re.split(TOC_EC3.refsepRegex,toc.ref)
+for reference in ECTocList.list:
+  levels=re.split(ECTocReferenceSystem.idSeparatorRegex,reference.id)
   levels=list(filter(lambda item: item, levels))
   levels=list(map(int,levels))
-  for level in range(toc.level+1):
+  print(levels)
+  for level in range(reference.level+1):
     numLevel=levels[level]
     #if numLevel=1 there is no previous ref in this level
     if numLevel!=1:
@@ -101,52 +138,194 @@ for toc in TOC_EC3.tocList:
       if level!=0:
         prevref=''
         for i in range(level):
-          prevref=prevref+str(levels[i])+TOC_EC3.refsep
+          prevref=prevref+str(levels[i])+ECTocReferenceSystem.idSeparator
           pass
       else:
         prevref=''
         pass
       #add to refMissing the lefts references
       for i in range(1,numLevel+1):
-        refMissing.append(prevref+str(i)+TOC_EC3.refsep)
+        refMissing.append(prevref+str(i)+ECTocReferenceSystem.idSeparator)
         pass
       pass
     pass
   pass
 refMissing=list(set(refMissing))
 refMissing.sort()
-print(refMissing)
-len(refMissing)
+
+ECTocList.print()
+
 
 # Add Missing toc's in TOC
-for ref in refMissing:
-  refsep=TOC_EC3.refsep
-  beginLocation=DeCoderAux.PDFlocation(-1)
-  toc_EC3=DeCoderAux.toc(ref,refsep,'',beginLocation)
-  TOC_EC3.addToc(toc_EC3)
+for refId in refMissing:
+  ECTocList.addReference(id=refId,gapActivation=False)
   pass
-TOC_EC3.tocList.sort(key=lambda x: x.ref, reverse=False)
-TOC_EC3.printTOC()
+ECTocList.sort('id')
 
-# Search Missing toc's in Main text
-pattern = re.compile(TOC_EC3.refRegex)
-refMissing=[]
-refTOC=[]
-for toc in TOC_EC3.tocList:
-  refTOC.append(toc.ref)
+# Pages References and their Levels
+pagesReferences={}
+pattern=re.compile(ECTocReferenceSystem.idRegex)
+for pageNum in range(0,numPages):
+  page=pages[pageNum]
+  refList=[]
+  newpage=page
+  for match in pattern.finditer(page):
+    refList.append([ECTocReferenceSystem.getId(match.group()),
+                    ECTocReferenceSystem.getLevel(match.group())])
+    pass
+  pagesReferences[len(pagesReferences.keys())]=refList
   pass
-for page in pages:
-  for match in pattern.finditer(pages[page]):
-    if match.group() not in refTOC:
-      refMissing.append(match.group())
+
+# Delete incorrect references locations in TOC
+for reference in ECTocList.list:
+  if reference.beginPage:
+    if reference.isInPage(reference.beginPage,pagesReferences) is False:
+      reference.deleteBegin()
       pass
     pass
   pass
 
-refMissing=list(set(refMissing))
-refMissing.sort()
-print(refMissing)
-len(refMissing)
+
+
+
+
+
+first=0
+final=1
+
+while final<len(ECTocList.list):
+  found=False
+  firstOfAll=first
+  print('first of all: {}'.format(firstOfAll))
+  # while not finding a missing page reference and till list end
+  while found==False and final<len(ECTocList.list):
+    firstReference=ECTocList.list[first]
+    finalReference=ECTocList.list[final]
+    if firstReference.beginPage is not None:
+      first+=1
+      final+=1
+    elif finalReference.beginPage is not None:
+      found=True
+    else:
+      final+=1
+      pass
+    pass
+  # if final at the end set final to last page
+  if final==len(ECTocList.list):
+    final-=1
+    finalReference=ECTocList.list[final]
+    pass
+  print('-----------------------------------------')
+  print(first,final)
+
+  # max between (last tocPages, firstReference TOC page)
+  if firstReference.beginPage:
+    firstPage=max(max(tocPages),firstReference.beginPage)
+  else:
+    firstPage=max(tocPages)
+    pass
+  # finalReference TOC page
+  if finalReference.beginPage:
+    lastPage=finalReference.beginPage
+  else:
+    lastPage=len(pages.keys())
+    pass
+
+  # Pages where references are
+  newTOC=False
+  # each reference between first and final
+  for ref in range(first,final):
+    reference=ECTocList.list[ref]
+    # list pages where reference is (after TOC & before final page)
+    pageList=[]
+    for pageNum in range(0,numPages):
+      if (reference.isInPage(pageNum,pagesReferences) and
+          pageNum<=lastPage and
+          pageNum>firstPage):
+        pageList.append(pageNum)
+        pass
+      pass
+    print(pageList)
+    if len(pageList)==1:
+      ECTocList.list[ref].beginPage=pageList[0]
+      newTOC=True
+      print('in {}  assign {}'.format(ref,pageList[0]))
+      pass
+    pass
+  # Update first and last if newTOC
+  if newTOC==True:
+    # if newTOC back to the first
+    first=firstOfAll
+  else:
+    # unless newTOC go forward
+    first=final
+    pass
+  final=first+1
+  pass
+
+# Get begin character
+for reference in ECTocList.list:
+  if reference.beginPage is not None:
+    reference.setBeginCharacter(pages)
+    pass
+  pass
+
+ECTocList.print()
+
+
+
+# Pages references
+pagesReferencesMatrix=np.zeros((numPages,len(ECTocList.list)))
+i=0
+while i<numPages:
+  j=0
+  while j<len(ECTocList.list):
+    print(i,j)
+    print(pages[i].count(ECTocList.list[j].id))
+    pagesReferencesMatrix[i,j]=pages[i].count(ECTocList.list[j].id)
+    j+=1
+    pass
+  pagesReferencesMatrix[i]=auxList
+  i+=1
+  pass
+
+
+
+
+
+# # Search complete references
+# i=0
+# found=False
+# while found==False:
+#   reference=ECTocList.list[i]
+#   if reference.isComplete():
+#     reference.print()
+#     locationPage=pages[int(reference.location)]
+#     locationPage.find(reference.id)
+#     found=True
+#     pass
+#   i+=1
+#   pass
+
+# # Search Missing toc's in Main text
+# pattern = re.compile(TOC_EC3.refRegex)
+# refMissing=[]
+# refTOC=[]
+# for toc in TOC_EC3.tocList:
+#   refTOC.append(toc.ref)
+#   pass
+# for page in pages:
+#   for match in pattern.finditer(pages[page]):
+#     if match.group() not in refTOC:
+#       refMissing.append(match.group())
+#       pass
+#     pass
+#   pass
+
+# refMissing=list(set(refMissing))
+# refMissing.sort()
+# print(refMissing)
+# len(refMissing)
 
 
 pdfFile.close()
--- DeCoder_Auxiliary.py
+++ DeCoder_Auxiliary.py
@@ -1,165 +1,289 @@
 import regex as re
 from tabulate import tabulate
 
-class ReferenceSystem():
-  def __init__(self,refidRegex,refsepRegex,refsep):
-    self.refidRegex=str(refidRegex)
-    self.refsepRegex=str(refsepRegex)
-    self.refsep=str(refsep)
-    self.refRegex=refidRegex+'(?:'+refsepRegex+'{1}'+refidRegex+'+)+'
-    pass
-  pass
-
-class Reference(ReferenceSystem):
-  def __init__(self,refSys,ref):
-    if isinstance(refSys,ReferenceSystem):
-      self.refSys=refSys
-      if re.search(self.refSys.refRegex)!=None:
-        self.ref=ref
-        self.level = len(list(filter(lambda item: item, re.split(self.refSys.refsep+'*',ref))))-1
-      else:
-        self.ref=None
-        self.level=None
-        pass
+class IdSystem():
+  # Constructor
+  def __init__(self,regex=None,
+               idIndicatorRegex=None,idSeparatorRegex=None,idSeparator=None):
+    if regex is not None:
+      self.idRegex=regex
+      self.idIndicatorRegex=None
+      self.idSeparatorRegex=None
+      self.idSeparator=None
+    elif idIndicatorRegex is not None and idSeparatorRegex is not None and idSeparator is not None:
+      self.idRegex=idIndicatorRegex+'(?:'+idSeparatorRegex+'{1}'+idIndicatorRegex+'+)+'
+      self.idIndicatorRegex=idIndicatorRegex
+      self.idSeparatorRegex=idSeparatorRegex
+      self.idSeparator=idSeparator
+    else:
+      self.idRegex=None
+      self.idIndicatorRegex=None
+      self.idSeparatorRegex=None
+      self.idSeparator=None
       pass
     pass
+  # Method: Get id level
+  def getLevel(self,id):
+    return len(list(filter(lambda item:item,re.split(self.idSeparatorRegex,id))))-1
+  # Method: Get real id
+  def getId(self,id):
+    levels=re.split(self.idSeparatorRegex,id)
+    levels=list(filter(lambda item: item, levels))
+    levels=list(map(int,levels))
+    newId=''
+    for level in levels:
+      newId=newId+'{:03}'.format(level)+self.idSeparator
+      pass
+    return newId
   pass
 
 class NameSystem():
   # Constructor
-  def __init__(self,namRegex):
-    self.namRegex=namRegex
+  def __init__(self,regex=None):
+    if regex is not None:
+      self.nameRegex=regex
+      pass
     pass
+  # Method: Get real name
+  def getName(self,name):
+    if name is None:
+      return None
+    else:
+      newName=name
+      newName=re.sub('\n','',newName)
+      newName=re.sub('\t','',newName)
+      return newName
   pass
 
-class Name(NameSystem):
-  def __init__(self,namSys,nam):
-    if isinstance(namSys,NameSystem):
-      NameSystem.__init__(self, namSys.namRegex)
-      if re.search(self.namRegex)!=None:
-        self.nam=nam
-      else:
-        self.nam=None
-        pass
+class SeparatorSystem():
+  # Constructor
+  def __init__(self,regex=None):
+    if regex is not None:
+      self.separatorRegex=regex
       pass
     pass
   pass
 
 class LocationSystem():
   # Constructor
-  def __init__(self,locRegex):
-    self.locRegex=locRegex
+  def __init__(self,regex=None,gapTocReal=0):
+    if regex is not None:
+      self.locationRegex=regex
+      self.gap=gapTocReal
+      pass
     pass
-  pass
-
-class Location(LocationSystem):
-  # Constructor
-  def __init__(self,locSys,page,begin=None,end=None):
-    if isinstance(locSys,LocationSystem):
-      self.page=int(page)
-      if begin is None:
-        self.begin=None
-        self.end =None
+  # Method: Get real page
+  def getPage(self,page,gapActivation=False):
+    if page is None:
+      return None
+    else:
+      if gapActivation==True:
+        return int(page)+self.gap
       else:
-        self.begin=int(begin)
-        if end is None:
-          self.end=int(begin)
-        else:
-          self.end=end
-          pass
-        pass
+        return int(page)
       pass
     pass
   pass
 
-class SeparatorSystem():
+class ReferenceSystem(IdSystem,NameSystem,SeparatorSystem,LocationSystem):
   # Constructor
-  def __init__(self,sepRegex):
-    self.sepRegex=sepRegex
+  def __init__(self,idSys=None,namSys=None,sepSys=None,locSys=None,nomatterRegex=None):
+    if (idSys is not None and isinstance(idSys,IdSystem),
+        namSys is not None and isinstance(namSys,NameSystem),
+        sepSys is not None and isinstance(sepSys,SeparatorSystem),
+        locSys is not None and isinstance(locSys,LocationSystem)):
+      IdSystem.__init__(self,idIndicatorRegex=idSys.idIndicatorRegex,
+                        idSeparatorRegex=idSys.idSeparatorRegex,
+                        idSeparator=idSys.idSeparator)
+      NameSystem.__init__(self,regex=namSys.nameRegex)
+      SeparatorSystem.__init__(self,regex=sepSys.separatorRegex)
+      LocationSystem.__init__(self,regex=locSys.locationRegex,
+                              gapTocReal=locSys.gap)
+      self.nomatterRegex=nomatterRegex
+      self.referenceRegex=(idSys.idRegex+nomatterRegex+
+                           namSys.nameRegex+nomatterRegex+
+                           sepSys.separatorRegex+nomatterRegex+
+                           locSys.locationRegex)
     pass
+  # Method: Is reference
+  def isReference(self,ref):
+    if ref is None:
+      return False
+    elif re.match(self.referenceRegex,ref) is not None:
+      return True
+    else:
+      return False
+  # Method: Print
+  def print(self):
+    print('idRegex: {0}'.format(self.idRegex))
+    print('idIndicatorRegex: {0}'.format(self.idIndicatorRegex))
+    print('idSeparatorRegex: {0}'.format(self.idSeparatorRegex))
+    print('referenceRegex: {0}'.format(self.referenceRegex))
   pass
 
-class Separator(SeparatorSystem):
-  def __init__(self,sepSys,sep):
-    if isinstance(sepSys,SeparatorSystem):
-      SeparatorSystem.__init__(self, sepSys.sepRegex)
-      if re.search(self.sepRegex)!=None:
-        self.sep=sep
-      else:
-        self.sep=None
-        pass
-      pass
-    pass
-  pass
-
-class TocSystem(ReferenceSystem,NameSystem,LocationSystem,SeparatorSystem):
-  def __init__(self,refSys,namSys,locSys,sepSys,noMatterRegex,noMatter):
-    if isinstance(refSys,ReferenceSystem) and isinstance(namSys,NameSystem) and isinstance(locSys,LocationSystem):
-      self.refSys=refSys
-      self.namSys=namSys
-      self.locSys=locSys
-      self.sepSys=sepSys
-      self.noMatterRegex=noMatterRegex
-      self.noMatter=noMatter
-      self.tocRegex=refSys.refRegex+noMatterRegex+namSys.namRegex+noMatterRegex+sepSys.sepRegex+noMatterRegex+locSys.locRegex
-      pass
-    pass
-  pass
-
-class TocUnit(TocSystem):
-  # Contructor
-  def __init__(self,reference,name,location,separator):
-    if isinstance(reference,Reference) and isinstance(name,Name) and isinstance(location,Location) and isinstance(separator,Separator):
-      self.reference=reference
-      self.name=name
-      self.location=location
-      self.separator=separator
-      pass
-    pass
-  pass
-
-class Toc():
+class Reference():
   # Constructor
-  def __init__(self,refSys,namSys,locSys,sepSys):
+  def __init__(self,refSys,ref,id=None,name=None,separator=None,
+               beginPage=None,beginCharacter=None,endPage=None,endCharacter=None,
+               gapActivation=False):
     if isinstance(refSys,ReferenceSystem):
       self.refSys=refSys
-    else:
-      self.refSys=None
+      if refSys.isReference(ref):
+        self.reference=ref
+        # id
+        self.id=self.refSys.getId(re.match(refSys.idRegex,ref).group())
+        ref=re.sub(refSys.idRegex,'',ref,count=1)
+        # level
+        self.level=refSys.getLevel(self.id)
+        # nomatter
+        ref=re.sub(refSys.nomatterRegex,'',ref,count=1)
+        # name
+        self.name=refSys.getName(re.match(refSys.nameRegex,ref).group())
+        ref=re.sub(refSys.nameRegex,'',ref,count=1)
+        # nomatter
+        ref=re.sub(refSys.nomatterRegex,'',ref,count=1)
+        # separator
+        self.separator=re.match(refSys.separatorRegex,ref).group()
+        ref=re.sub(refSys.separatorRegex,'',ref,count=1)
+        # nomatter
+        ref=re.sub(refSys.nomatterRegex,'',ref,count=1)
+        # beginPage
+        self.beginPage=refSys.getPage(re.match(refSys.locationRegex,ref).group(),gapActivation=gapActivation)
+        ref=re.sub(refSys.locationRegex,'',ref,count=1)
+      else:
+        self.id=self.refSys.getId(id)
+        self.level=refSys.getLevel(self.id)
+        self.name=self.refSys.getName(name)
+        self.separator=separator
+        self.beginPage=self.refSys.getPage(beginPage,gapActivation=gapActivation)
+        self.beginCharacter=beginCharacter
+        self.endPage=self.refSys.getPage(endPage,gapActivation=gapActivation)
+        self.endCharacter=endCharacter
+        pass
       pass
-    if isinstance(namSys,NameSystem):
-      self.namSys=namSys
-    else:
-      self.namSys=None
-      pass
-    if isinstance(locSys,LocationSystem):
-      self.locSys=locSys
-    else:
-      self.locSys=None
-      pass
-    if isinstance(sepSys,SeparatorSystem):
-      self.sepsys=sepSys
-    else:
-      self.sepsys=None
-      pass
-    self.tocList=[]
     pass
-  # Method: Add toc to TOC.tocList
-  def addToc(self,toc):
-    if isinstance(toc,TocUnit):
-      self.tocList.append(toc)
-      pass
+  # Method: Is complete
+  def isComplete(self):
+    if (self.id is not None and
+        self.level is not None and
+        self.name is not None and
+        self.separator is not None and
+        self.beginPage is not None and
+        self.beginCharactern is not None and
+        self.endPage is not None and
+        self.endCharacter is not None):
+      return True
+    else:
+      return False
     pass
-  # Method: Print TOC.tocList in a table
-  def printToc(self,num=None):
+  # Method: Is in page
+  def isInPage(self,pageToSearch,pagesReferences):
+    if self.id in [x[0] for x in pagesReferences[pageToSearch]]:
+      return True
+    else:
+      return False
+    pass
+  # Method: Set begin character
+  def setBeginCharacter(self,pages):
+    self.beginCharacter=pages[self.beginPage].find(self.id)
+    pass
+
+  # Method: Delete Begin
+  def deleteBegin(self):
+    self.beginPage=None
+    self.beginCharacter=None
+    pass
+  # Method: Delete End
+  def deleteEnd(self):
+    self.endPage=None
+    self.endCharacter=None
+    pass
+  # Method: Print
+  def print(self):
     table=[]
-    if num is None:
-      for toc in self.tocList:
-        table.append([toc.ref,toc.name,toc.begin.page,toc.level])
+    table.append([self.level,self.id,self.beginPage,self.beginCharacter,self.endPage,self.endCharacter])
+    print(tabulate(table,headers=['level','id','beginPage','beginCharacter','endPage','endCharacter'], tablefmt='orgtbl'))
+    pass
+  pass
+
+class ReferenceList():
+  # Constructor
+  def __init__(self,refSys):
+    if isinstance(refSys,ReferenceSystem):
+      self.refSys=refSys
+      self.list=[]
+      pass
+    pass
+  # Method: Add Reference
+  def addReference(self,ref=None,id=None,name=None,separator=None,beginPage=None,
+                   beginCharacter=None,endPage=None,endCharacter=None,
+                   gapActivation=False):
+    refToAdd=Reference(self.refSys,ref,id=id,name=name,separator=separator,
+                       beginPage=beginPage,beginCharacter=beginCharacter,endPage=endPage,endCharacter=endCharacter
+                       ,gapActivation=gapActivation)
+    if refToAdd.id not in self.getList('id'):
+      self.list.append(refToAdd)
+      pass
+    pass
+  # Method: Sort List
+  def sort(self,by='id'):
+    if by=='id':
+      self.list.sort(key=lambda x: x.id, reverse=False)
+    elif by=='level':
+      self.list.sort(key=lambda x: x.level, reverse=False)
+    elif by=='name':
+      self.list.sort(key=lambda x: x.name, reverse=False)
+    elif by=='separator':
+      self.list.sort(key=lambda x: x.separator, reverse=False)
+    elif by=='beginPage':
+      self.list.sort(key=lambda x: x.beginPage, reverse=False)
+    elif by=='beginCharacter':
+      self.list.sort(key=lambda x: x.beginCharacter, reverse=False)
+    elif by=='endPage':
+      self.list.sort(key=lambda x: x.endPage, reverse=False)
+    elif by=='endCharacter':
+      self.list.sort(key=lambda x: x.endCharacter, reverse=False)
+      pass
+    pass
+  # Method: Get list of references attribute
+  def getList(self,attribute=None):
+    attributeList=[]
+    if attribute=='id':
+      attributeList=(ref.id for ref in self.list)
+    elif attribute=='level':
+      attributeList=(ref.level for ref in self.list)
+    elif attribute=='name':
+      attributeList=(ref.name for ref in self.list)
+    elif attribute=='separator':
+      attributeList=(ref.separator for ref in self.list)
+    elif attribute=='beginPage':
+      attributeList=(ref.beginPage for ref in self.list)
+    elif attribute=='beginCharacter':
+      attributeList=(ref.beginCharacter for ref in self.list)
+    elif attribute=='endPage':
+      attributeList=(ref.endPage for ref in self.list)
+    elif attribute=='endCharacter':
+      attributeList=(ref.endCharacter for ref in self.list)
+      pass
+    return attributeList
+  # Method: Print
+  def print(self,first=None,last=None):
+    table=[]
+    if first is None and last is None:
+      for reference in self.list:
+        table.append([reference.level,reference.id,
+                      reference.beginPage])#,reference.beginCharacter
         pass
     else:
-      toc=self.tocList[num]
-      table.append([toc.ref,toc.name,toc.begin.page,toc.level])
+      if last is None:
+        last=first
+        pass
+      for i in range(first,last+1):
+        reference=self.list[i]
+        table.append([reference.level,reference.id,
+                      reference.beginPage,reference.beginCharacter])
+        pass
       pass
-    print(tabulate(table,headers=['Ref.','Name','Begin page','Level'], tablefmt='orgtbl'))
+    print(tabulate(table,headers=['level','id','begPag','begChar'], tablefmt='github'))#tablefmt='orgtbl'))
     pass
   pass
